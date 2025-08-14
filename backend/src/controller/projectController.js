4f1c8ac5d5b53387f4d0328badcc2a200d209851
sxhilx <jadasahil786@gmail.com>
2025-08-14 16:41:43 +0200

    feat: add getProject model and controller

--- backend/src/controller/projectController.js
+++ backend/src/controller/projectController.js
@@ -1,15 +1,20 @@
 import { StatusCodes } from "http-status-codes"
-import { getAllProjectsService } from "../models/projectModel.js"
+import { getAllProjectsService, getProjectService } from "../models/projectModel.js"
+import { BadRequestError } from '../errors/index.js';
+
 
 export const getAllProjects = async (req, res) => {
-    console.log(req.query.limit);
-
     const {limit, offset} = req.query || 12
     const projects = await getAllProjectsService(limit, offset)
     res.status(StatusCodes.OK).json(projects)
 }
 export const getProject = async (req, res) => {
-    res.send('get a project')
+    const {id} = req.params;
+    if(!id){
+        throw new BadRequestError("ID is required")
+    }
+    const project = await getProjectService(id)
+    res.status(StatusCodes.OK).json(project)
 }
 export const addProject = async (req, res) => {
     res.send('share project')
--- backend/src/models/projectModel.js
+++ backend/src/models/projectModel.js
@@ -1,4 +1,6 @@
 import pool from "../config/db.js"
+import { BadRequestError } from '../errors/index.js';
+
 
 
 
@@ -11,11 +13,17 @@ export const getAllProjectsService = async (limit, offset) => {
 }
 
 export const getProjectService = async (projectId) => {
+    const project = await pool.query("SELECT * FROM projects WHERE id=$1", [projectId])
 
+    if(!project || project.rows.length === 0){
+        throw new BadRequestError("Project with this ID does not exists")
+    }
+
+    return {project: project.rows[0]}
 }
 
 export const addProjectService = async (projectData) => {
-
+    
 }
 
 export const deleteProjectService = async (projectId) => {
