8a28f84db393bce5f4c16f282dc7f3ba9443b358
sxhilx <jadasahil786@gmail.com>
2025-08-16 14:40:43 +0200

    fix: add project name and tagline with the notification

--- backend/src/models/notificationModel.js
+++ backend/src/models/notificationModel.js
@@ -2,9 +2,12 @@ import pool from "../config/db.js"
 import { NotFoundError } from "../errors/index.js"
 
 export const getAllNotificationsService = async (userId) => {
-    const notifications = await pool.query(`SELECT n.*, u.username AS actor_username FROM notifications n
+    const notifications = await pool.query(`SELECT n.*, u.username AS actor_username, p.title AS project_title, p.tagline AS project_tagline
+                                            FROM notifications n
                                             JOIN users u ON n.actor_id = u.id
-                                            WHERE n.user_id=$1 ORDER BY created_at DESC`, [userId])
+                                            LEFT JOIN projects p ON n.project_id = p.id
+                                            WHERE n.user_id=$1
+                                            ORDER BY n.created_at DESC`, [userId])
 
     if(!notifications.rows){
         throw new NotFoundError("No notifications found")
