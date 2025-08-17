a8362cff4809194016c0f7afb1a0d6ed50676e7d
sxhilx <jadasahil786@gmail.com>
2025-08-17 16:59:46 +0200

    fix: add request status with notification

--- backend/src/models/notificationModel.js
+++ backend/src/models/notificationModel.js
@@ -2,12 +2,12 @@ import pool from "../config/db.js"
 import { NotFoundError } from "../errors/index.js"
 
 export const getAllNotificationsService = async (userId) => {
-    const notifications = await pool.query(`SELECT n.*, u.username AS actor_username, p.title AS project_title, p.tagline AS project_tagline
+    const notifications = await pool.query(`SELECT n.*, u.username AS actor_username, p.title AS project_title, p.tagline AS project_tagline, pr.status
                                             FROM notifications n
                                             JOIN users u ON n.actor_id = u.id
                                             LEFT JOIN projects p ON n.project_id = p.id
                                             LEFT JOIN project_requests pr ON n.request_id = pr.id
-                                            WHERE n.user_id=$1 AND (n.request_id IS NULL OR pr.status='pending')
+                                            WHERE n.user_id=$1
                                             ORDER BY n.created_at DESC`, [userId])
 
     if(!notifications.rows){
